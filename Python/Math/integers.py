a5851b9bc9b839f3b263950dfc9ca66b467fbe22
Jennifer Cafiero <jcafiero@stevens.edu>
2017-04-30 19:24:30 -0400

    Adds solution to Integers Come in All Sizes

--- Python/Math/integers.py
+++ Python/Math/integers.py
@@ -1,3 +1,10 @@
 #Author: Jennifer Cafiero
 #Date: April 30 2017
 #HackerRank Python - Integers Come in All Sizes
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print (pow(a,b) + pow(c,d))
